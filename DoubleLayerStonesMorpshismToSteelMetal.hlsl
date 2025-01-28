e7ca45e188d7b3a701d16e79c774fb4564865c8e
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-28 14:54:50 +0100

    Create DoubleLayerStonesMorpshismToSteelMetal.hlsl

---
+++ DoubleLayerStonesMorpshismToSteelMetal.hlsl
@@ -0,0 +1,8 @@
+//we encounter a steel metal.
+//On the other hand, lets use morphisms to transform hankel datas to differential models. That said, this is perfectly suited for many things !
+
+
+float getInheritanceCGAL_GPU_forCrashProbe3D(...)
+{
+
+}
